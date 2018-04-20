362290ccb1c7b7fe28a533a1202d0fea76d03120
M Haidar Hanif <git@mhaidarhanif.com>
2018-04-20 13:53:47 +0700

    Setup basic Express app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,33 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
 
+import * as express from "express";
+import * as cors from "cors";
+import * as morgan from "morgan";
+import * as bodyParser from "body-parser";
+
 // connection settings are in the "ormconfig.json" file
 createConnection()
   .then(async connection => {
     // EXPRESS CODE HERE
+
+    // create express app
+    const app = express();
+    const PORT = process.env.PORT || 3000;
+
+    app.use(cors());
+    app.use(morgan("combined"));
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    // register all application routes
+    app.use("/", RootRoutes);
+    app.use("/todos", TodoRoutes);
+    app.use("/users", UserRoutes);
+
+    // run app
+    app.listen(PORT, () => {
+      console.log(`Impact Todo API is listening on :${PORT}`);
+    });
   })
   .catch(error => console.log("Error: ", error));
